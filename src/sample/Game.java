ee17dc16116140de625fcb83d0ce324dc6c56918
T I T A N <ali.albabily8@gmail.com>
2018-10-26 12:04:27 +0200

    New methods
    Added new methods & variables.

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -2,7 +2,58 @@ package sample;
 
 public class Game {
 
+    boolean playerTurnToggle;
+
+    private int roundCounter = 0;
+    private int turnCounter = 0;
+    List<GameCard> allCards;
+
     public Game() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+    }
+
+    public toggleTurn() {
+
+    }
+
+    public print() {
+
+    }
+
+    public printWinningMessage() {
+
+    }
+
+    public playCard(Card card) {
+
+    }
+
+    public attack(Card card, Card card2) {
+
+    }
+
+    public attackPlayer() {
+
+    }
+
+    public getUserInput() {
+
+    }
+
+    public checkDeath() {
+
+    }
+
+    public roundCheck() {
+
+    }
+
+    public killPlayer() {
+
+    }
+
+    public randomNumber() {
 
     }
 }

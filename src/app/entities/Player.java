1a32a35993782d8a3a6ab69244d26ef0b0d5ecf2
Robin <robin.larsson040@gmail.com>
2018-10-30 15:01:47 +0100

    give m yfriends latest version

--- src/app/entities/Player.java
+++ src/app/entities/Player.java
@@ -63,6 +63,12 @@ public class Player {
     }
 
     public void drawCard() {
+        if(cardsInDeck.size() > 0){
+            cardsOnHand.add(cardsInDeck.get(cardsInDeck.size() -1));
+            cardsOnHand.remove(cardsOnHand.size() - 1);
+        }else{
+            System.out.println("can't draw card!");
+        }
 
     }
 

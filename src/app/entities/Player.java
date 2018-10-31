6827b2ce30e40348ea5a6e0f88a8d8d004c466bf
Calle Schönberg <calleschonberg@gmail.com>
2018-10-31 12:29:21 +0100

    Adds logic for delete a card from cardsOnTable list

--- src/app/entities/Player.java
+++ src/app/entities/Player.java
@@ -77,7 +77,7 @@ public class Player {
     }
 
     public void killCard(String cardName) {
-
+        cardsOnTable.removeIf(card -> card.getName().equals(cardName));
     }
 
     public void reduceHp(int damage) {

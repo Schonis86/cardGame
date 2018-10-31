b539967e91a2c14c4a20ae7c1122cbc8cb22498e
Anna-Karin Friberg <anna-karin@getmeetio.com>
2018-10-31 13:23:46 +0100

    Wrote tests and logic for GameCard increase and decrease HP and changed test for Player increase and decrease HP.

--- src/test/GameCardTest.java
+++ src/test/GameCardTest.java
@@ -1,17 +1,44 @@
 package test;
 
+import app.entities.GameCard;
+import app.entities.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameCardTest {
 
+    GameCard card;
+
+    @BeforeEach
+    void setUp() {
+        card = new GameCard("card1");
+    }
+
     @Test
     void increaseHp() {
+        int hpBefore = card.getHp();
+        final int HEAL = 10;
+
+        card.increaseHp(HEAL);
+        int hpAfter = card.getHp();
+
+        assertEquals(hpAfter - 10, hpBefore);
     }
 
     @Test
     void decreaseHp() {
+        int hpBefore = card.getHp();
+        final int DAMAGE = 5;
+
+        card.decreaseHp(DAMAGE);
+        int hpAfter = card.getHp();
+
+        assertEquals(hpAfter, hpBefore-DAMAGE);
     }
 
     @Test

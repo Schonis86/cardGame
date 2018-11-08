9f75643d8bdab7b09ae04be7172fc3fa50220b44
Lina <linamattsson@live.se>
2018-11-08 16:27:34 +0100

    added switch/case to enum, so the right method will be called

--- src/main/java/app/MagicMethod.java
+++ src/main/java/app/MagicMethod.java
@@ -14,11 +14,30 @@ public enum MagicMethod {
         this.methodNumber = methodNumber;
     }
 
-    public int getMethodNumber() {
+    private int getMethodNumber() {
         return methodNumber;
     }
 
     public void doMagicMethod(){
-        
+        switch (getMethodNumber()){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+        }
     }
 }

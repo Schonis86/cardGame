d44c54382ff79406f87bd7d12902f639eafbdc44
T I T A N <ali.albabily8@gmail.com>
2018-11-08 11:07:50 +0100

    edited MagicCard Class

--- src/main/java/app/entities/MagicCard.java
+++ src/main/java/app/entities/MagicCard.java
@@ -11,4 +11,19 @@ public class MagicCard {
     public void setName(String name) {
         this.name = name;
     }
+
+    //omedelbar effekt - öka Player1/2 HP med 3
+    public void selfHeal() {
+
+    }
+
+    // riktad effek - ge direkt skada till motståndarens kort med 2
+    public void damageEnemyCards() {
+
+    }
+
+    // riktad effek - öka ett korts Hp med 2
+    public void increaseCradHp() {
+
+    }
 }

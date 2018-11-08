f6e35fc3f84a120eb34e23e424558668577dee62
Lina <linamattsson@live.se>
2018-11-08 11:50:29 +0100

    new dev to merge

--- src/main/java/app/AttackType.java
+++ src/main/java/app/AttackType.java
@@ -3,7 +3,14 @@ package app;
 import com.sun.deploy.security.WinDeployNTLMAuthCallback;
 
 public enum AttackType {
-    FIRE
-    WIND
-    WATER
+    FIRE (1, 2, 3),
+    WIND (2, 3, 1),
+    WATER(3, 1, 2);
+
+    private final double mass;   // in kilograms
+    private final double radius; // in meters
+    Planet(double mass, double radius) {
+        this.mass = mass;
+        this.radius = radius;
+    }
 }
